8128a2619bd991b52df6472d980a7d4577dc2106
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-05-16 23:23:15 -0300

    Criação dos controllers de Cliente e Plano.

---
+++ Estudo01/Estudo01/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Estudo01.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}
