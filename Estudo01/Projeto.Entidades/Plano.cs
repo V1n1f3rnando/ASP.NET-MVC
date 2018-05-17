ec71e4bc827a6ba01b5adaabb7a82399a158f2a9
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-05-16 22:30:52 -0300

    Criação das entidades cliente e plano.

---
+++ Estudo01/Projeto.Entidades/Plano.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades
+{
+    public class Plano
+    {
+        public int IdPlano { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public Plano()
+        {
+
+        }
+    }
+}
---
+++ Estudo01/Projeto.Entidades/Tipos/EstadoCivil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades.Tipos
+{
+    public enum EstadoCivil
+    {
+        Solteiro = 0,
+        Casado = 1,
+        Divorciado = 2,
+        Viúvo = 3
+
+    }
+}
