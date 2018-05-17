a75e879db5f123f37ee633b7ed3f7d123ca1bb91
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-05-16 23:56:35 -0300

    Criando método para conexão com o banco.

--- Estudo01/Projeto.DAL/Conexao.cs
+++ Estudo01/Projeto.DAL/Conexao.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Projeto.DAL
 {
-    public class Conexao
+    public class Conexao 
     {
+        protected SqlConnection con;
+        protected SqlCommand cmd;
+        protected SqlDataReader dr;
+        protected SqlTransaction tr;
 
+        //Método para abrir a conexão com o banco.
+        protected void AbrirConexao()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["banco"]
+                .ConnectionString);
+            con.Open(); //conectado
+        }
+
+        protected void FecharConexao()
+        {
+            con.Close(); //desconectado
+        }
     }
 }
