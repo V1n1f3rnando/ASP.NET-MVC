6fe87323cf815e829b948a53fc635958314afac3
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-25 08:42:40 -0300

    Usando razor para criar o formulário de cadastro de planos.

---
+++ Projeto01/Projeto.WEB/Models/PlanoCadastroViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //validações
+
+namespace Projeto.WEB.Models
+{
+    public class PlanoCadastroViewModel
+    {
+        [MinLength(3, ErrorMessage ="Por favor, informe no mínimo {1} caracteres")]
+        [MaxLength(50, ErrorMessage = "Por favor, informe no máximo {1} caracteres")]
+        [Required(ErrorMessage = "Por favor, informe o nome do plano.")]
+        public string Nome { get; set; }
+
+        [MinLength(3, ErrorMessage = "Por favor, informe no mínimo {1} caracteres")]
+        [MaxLength(250, ErrorMessage = "Por favor, informe no máximo {1} caracteres")]
+        [Required(ErrorMessage = "Por favor, informe o nome do plano.")]
+        public string Descricao { get; set; }
+    }
+}
