b897a8ae753e8537fd3e7abb56f615ba074de638
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-25 07:30:11 -0300

    Criação do banco mdf e da entidade Plano.

--- Projeto01/Projeto01.Entidades/Plano.cs
+++ Projeto01/Projeto01.Entidades/Plano.cs
@@ -8,6 +8,31 @@ namespace Projeto01.Entidades
 {
     public class Plano
     {
+        public int IdPlano { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
 
+        //associação (TER-Muitos)
+        public List<Cliente> Cliente { get; set; }
+
+        //construtor
+        public Plano()
+        {
+
+        }
+
+        //sobrecarga do construtor 
+        public Plano(int idPlano, string nome, string descricao)
+        {
+            IdPlano = idPlano;
+            Nome = nome;
+            Descricao = descricao;
+        }
+
+        //sobrescrevendo o método ToString da classe object
+        public override string ToString()
+        {
+            return string.Format("Id:{0}, Plano:{1}, Descição:{2}", IdPlano, Nome, Descricao);
+        }
     }
 }
