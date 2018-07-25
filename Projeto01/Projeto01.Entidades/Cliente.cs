acf1d4baa7f4001131af74c6c52069f8a87a873f
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-25 07:23:55 -0300

    Criação da entidade Cliente.

---
+++ Projeto01/Projeto01.Entidades/Cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto01.Entidades.Tipos; // importando 
+
+namespace Projeto01.Entidades
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public Sexo Sexo { get; set; }
+        public EstadoCivil EstadoCivil { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        // relacionamento de associação (Ter-1)
+        public Plano Plano { get; set; }
+
+        //Construtor
+        public Cliente()
+        {
+
+        }
+
+        //Sobrecarregando o construtor 
+        public Cliente(int idCliente, string nome, string email, Sexo sexo, EstadoCivil estadoCivil, DateTime dataCadastro)
+        {
+            IdCliente = idCliente;
+            Nome = nome;
+            Email = email;
+            Sexo = sexo;
+            EstadoCivil = estadoCivil;
+            DataCadastro = dataCadastro;
+        }
+
+        public override string ToString()
+        {
+            return $"Id:{IdCliente}\n Nome:{Nome}\n Email:{Email}\n" +
+                $" Sexo:{Sexo}\n EstadoCivil:{EstadoCivil}\n Data de cadastro:{DataCadastro}";
+        }
+    }
+}
