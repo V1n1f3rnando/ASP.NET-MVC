088c724b63326ae3877712fe01ebf82d28d12d06
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-26 07:54:03 -0300

    Finalizando página de cadastro de cliente

---
+++ Projeto01/Projeto.WEB/Models/ClienteCadastroViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Projeto01.Entidades.Tipos;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.WEB.Models
+{
+    public class ClienteCadastroViewModel
+    {
+
+        //Validações
+        [Required(ErrorMessage = "Por favor, informe um nome.")]
+        [MinLength(3, ErrorMessage = "Por favor, informe no mínimo {1} caracteres")]
+        [MaxLength(50, ErrorMessage = "Por favor, informe no máximo {1} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe um email.")]
+        [EmailAddress(ErrorMessage = "Por favor, informe um email válido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o sexo do cliente")]
+        public Sexo Sexo { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o estado civíl do cliente")]
+        public EstadoCivil EstadoCivil { get; set; }
+
+    }
+}
