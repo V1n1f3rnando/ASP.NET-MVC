cb1444e53edc04e84a378dfafcff9b2bda39aa6c
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-07-26 08:36:12 -0300

    Formulário de cadastro de cliente.

--- Projeto01/Projeto.WEB/Controllers/ClienteController.cs
+++ Projeto01/Projeto.WEB/Controllers/ClienteController.cs
@@ -3,20 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Projeto01.Entidades;
+using Projeto.WEB.Models;
+using Projeto.DAL.Repositorios;
+using Projeto01.Entidades.Tipos;
 
 namespace Projeto.WEB.Controllers
 {
     public class ClienteController : Controller
     {
         // GET: Cliente
-        public ActionResult Cadastro()
+        public ActionResult Cadastro( )
         {
-            return View();
+            ClienteCadastroViewModel viewModel = new ClienteCadastroViewModel();
+            viewModel.ListagemDePlanos = ObterPlanos();
+
+            //Enviando a model para a página
+            return View(viewModel);
         }
+
+        //Post: Cliente/Cadastro
+        [HttpPost]
+        public ActionResult Cadastro(ClienteCadastroViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //mensagem 
+                ViewBag.Mensagem = "Cliente cadastrado com sucesso.";
+            }
+
+            ClienteCadastroViewModel viewModel = new ClienteCadastroViewModel();
+            viewModel.ListagemDePlanos = ObterPlanos();
+
+            return View(viewModel);
+        }
+
 
         public ActionResult Consulta()
         {
             return View();
+        }
+
+        //Método para obter os planos
+        private List<SelectListItem> ObterPlanos()
+        {
+            List<SelectListItem> lista = new List<SelectListItem>();
+
+            //acessando a base de dados..
+            PlanoRepositorio rep = new PlanoRepositorio();
+            foreach (var p in rep.Buscar())
+            {
+                SelectListItem item = new SelectListItem();
+                item.Value = p.IdPlano.ToString();
+                item.Text = p.Nome;
+
+                lista.Add(item);
+            }
+
+            return lista;
         }
     }
 }
--- Projeto01/Projeto.WEB/Models/ClienteCadastroViewModel.cs
+++ Projeto01/Projeto.WEB/Models/ClienteCadastroViewModel.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using Projeto01.Entidades.Tipos;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Projeto.WEB.Models
 {
     public class ClienteCadastroViewModel
     {
+        private List<SelectListItem> _listagemDePlanos;
 
         //Validações
         [Required(ErrorMessage = "Por favor, informe um nome.")]
@@ -26,5 +28,10 @@ namespace Projeto.WEB.Models
         [Required(ErrorMessage = "Por favor, informe o estado civíl do cliente")]
         public EstadoCivil EstadoCivil { get; set; }
 
+        [Required(ErrorMessage = "Selecione o plano desejado.")]
+        public int IdPlano { get; set; }
+
+        //Lista de planos no DropDown
+        public List<SelectListItem> ListagemDePlanos { get; set; }
     }
 }
