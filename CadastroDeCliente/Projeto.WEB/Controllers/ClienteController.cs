b217dae905649a42654310c647f1fac8aa5581a2
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-08-01 08:46:43 -0300

    Função JQuery.

---
+++ CadastroDeCliente/Projeto.WEB/Controllers/ClienteController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto.WEB.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente/Cadastro
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        // GET: Cliente/Consulta
+        public ActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}
