e312d558425b5967b9e03132a1b1af58928cdb56
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2019-01-02 22:36:03 -0200

    Páginas de cadastro e login com razor

---
+++ AutenticaUsuario/AutenticaUsuario.WEB/Models/UsuarioCadastroViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutenticaUsuario.WEB.Models
+{
+    public class UsuarioCadastroViewModel
+    {
+        [Required(ErrorMessage = "Informe o seu nome !")]
+        [Display(Name = "Nome do usuário")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o login desejado !")]
+        [Display(Name = "Login de acesso")]
+        public string Login { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Informe a senha de acesso !")]
+        [Display(Name = "Senha de acesso")]
+        public string Senha { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        [Required(ErrorMessage = "Por favor confirme a sua senha !")]
+        [Display(Name = "Confirmação de senha")]
+        public string SenhaConfirm { get; set; }
+    }
+}
---
+++ AutenticaUsuario/AutenticaUsuario.WEB/Models/UsuarioLoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutenticaUsuario.WEB.Models
+{
+    public class UsuarioLoginViewModel
+    {
+        [Required(ErrorMessage ="Informe o login de acesso !")]
+        [Display(Name ="Login de acesso")]
+        public string Login { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Informe a sua senha !")]
+        [Display(Name = "Senha de acesso")]
+        public string Senha { get; set; }
+    }
+}
